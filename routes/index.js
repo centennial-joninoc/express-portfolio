8eefaf8b97a73663746ff7ba4db351e707b409f2
John Celso Inocente <jinocen1@my.centennialcollege.ca>
2023-09-26 21:49:12 -0400

    Initial commit of express portfolio

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('pages/index', { title: 'Fishbone Studios' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('pages/about', { title: 'Fishbone Studios | About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res) {
+  res.render('pages/projects', { title: 'Fishbone Studios | Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res) {
+  res.render('pages/services', { title: 'Fishbone Studios | Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res) {
+  res.render('pages/contact', { title: 'Fishbone Studios | Contact' });
+});
+
+module.exports = router;
